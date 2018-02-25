b6f361723c12dca6f7061e48e05dd586097378ef
Rafael Kueng <rafi.kueng@gmx.ch>
2018-02-25 19:38:41 +0100

    setup dummy gunicorn app

--- setup_config/function_spawn_machine_setup/splapp.sh
+++ setup_config/function_spawn_machine_setup/splapp.sh
@@ -1,21 +1,60 @@
 #!/bin/bash
 
-# FUNCTION rabbitmqbroker SETUP SCRIPT
+# FUNCTION splapp SETUP SCRIPT
 #
 # reminder: this runs as sudo
 
 
-apt-get install -y --force-yes rabbitmq-server
+apt install -y libevent-2.0-5
+apt install -y python-greenlet python-eventlet python-gevent
 
-rabbitmqctl add_user ${functions.rabbitmq.user} ${SECRET.rabbitmq.psw.user}
-rabbitmqctl add_vhost ${functions.rabbitmq.vhost}
+apt install -y gunicorn 
 
-rabbitmqctl change_password guest ${SECRET.rabbitmq.psw.guest}
+apt install -y python-pip virtualenv
 
-rabbitmqctl set_permissions -p ${functions.rabbitmq.vhost} ${functions.rabbitmq.user} ".*" ".*" ".*"
-rabbitmqctl set_permissions -p ${functions.rabbitmq.vhost} guest ".*" ".*" ".*"
+mkdir -p ${functions.splapp.appdir}
 
-systemctl stop rabbitmq-server
-systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
+#TODO: get real app
+cat <<EOT > "${functions.splapp.appdir}/test.py"
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = 'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type','text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
+
+EOT
+
+
+cat <<EOT > /etc/systemd/system/gunicorn.service
+[Unit]
+Description=gunicorn daemon
+After=network.target
+
+[Service]
+PIDFile=/run/gunicorn/pid
+User=debian  
+Group=debian  
+RuntimeDirectory=gunicorn
+WorkingDirectory=${functions.splapp.appdir}
+ExecStart=/usr/bin/gunicorn \
+    --pid /run/gunicorn/pid   \
+    --workers 3 \
+    --bind 10.0.2.1:8001 \ 
+    test:app
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s TERM $MAINPID
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+systemctl start gunicorn
+systemctl enable gunicorn
+
 
